65fc4aca9241eeb2c2a78a77b6394ebf8d1b56b5
Karan Naik <karannaik7725@gmail.com>
2025-06-14 22:38:00 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,62 @@
+#create a list 
+#list=[1,2,3,4,5]
+#print(list)
+
+
+#indexing
+#print(list[2])
+
+#sclicing
+#print(list[1:2])
+#adding value in string
+"""list.append(6)
+list.pop()"""
+
+
+
+#tuple
+#list
+"""student[1,2,3]
+student[3]=4
+print(student)"""
+
+#dictionary
+
+"""d={"xyz":30,"abc":40,"ddd":50}
+##print(d)
+#e={1:20,2:30}
+#print(e)#
+
+for i,j in d.items():
+    print(f"index{i}:value{j}")"""
+
+
+#set 
+
+#task : 
+#creat a list :
+frint=["apple","bannana","mango","orange","pineapple"]
+print(frint)
+
+#2
+cor =(25.23456,15.5789592)
+print(cor)
+
+#3
+
+student={1,"xyz","comp"}
+print(student)
+
+#4
+sst={3,5,4,6,7}
+print(sst)
+#5
+""""list[1,2,3,4]
+list.append(5)
+print(list)#somting is wrong in this code check i letter"""
+
+T=(1,2,3,4,5,6,7,8)
+print(T[2])
+
+
+
---
+++ deffff.py
@@ -0,0 +1,51 @@
+#def sumvalue(a,b):
+    #c= a+b
+    #return c
+#print(sumvalue(10,20))
+
+#a=10
+#def value():
+   # return a
+#print(value())
+
+
+a=10
+def value():
+    global a 
+    a=0
+    return a
+print(value())
+
+
+
+#def value(*args):
+ #   return sum(args)
+#print(value(1,2,3,4,5))
+
+
+
+#def Dprint(**keyword):
+   # for i,j in keyword.items():
+      #  print(i,j)
+#a={1:20,2:30}
+#Dprint(name="karan",marks=100)
+
+
+
+
+
+##lamda
+#learn all at home
+
+
+
+
+
+
+
+#docstring
+
+#function
+#agruments
+#  
+
